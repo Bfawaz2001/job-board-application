d856f8fe2f0cd4c720bd094200b3ca50a81d5f8a
Bilal Fawaz <69845836+Bfawaz2001@users.noreply.github.com>
2024-10-06 17:56:42 +0100

    Add Job model and register it in the admin panel

--- jobboard/urls.py
+++ jobboard/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('jobs.urls')),  # Add this line
+    path('', include('jobs.urls')),
 ]
--- jobs/urls.py
+++ jobs/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Add this route
+    path('', views.home, name='home'),
 ]
