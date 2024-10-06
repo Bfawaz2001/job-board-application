6b16702a43fdc898ec96b465ee964846d855a6d8
Bilal Fawaz <69845836+Bfawaz2001@users.noreply.github.com>
2024-10-06 19:35:56 +0100

    Add job posting functionality

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,8 +1,10 @@
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('job/<int:pk>/', views.job_detail, name='job_detail'),  # Job detail route
+    path('job/<int:pk>/', views.job_detail, name='job_detail'),
+    path('post-job/', views.post_job, name='post_job'),  # New URL for posting jobs
 ]
 
--- jobs/views.py
+++ jobs/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+from django.shortcuts import redirect
 from .models import Job
-
+from .forms import JobForm
 
 def home(request):
     jobs = Job.objects.all()
@@ -14,3 +15,14 @@ def home(request):
 def job_detail(request, pk):
     job = get_object_or_404(Job, pk=pk)
     return render(request, 'jobs/job_detail.html', {'job': job})
+
+
+def post_job(request):
+    if request.method == 'POST':
+        form = JobForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = JobForm()
+    return render(request, 'jobs/post_job.html', {'form': form})
