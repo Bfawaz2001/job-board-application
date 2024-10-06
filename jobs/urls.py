f531ae73e7f5e2d184e51ee8fac6fdc4420cc5d0
Bilal Fawaz <69845836+Bfawaz2001@users.noreply.github.com>
2024-10-06 18:29:38 +0100

    Add job listing and detail views with templates

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('job/<int:pk>/', views.job_detail, name='job_detail'),  # Job detail route
 ]
+
--- jobs/views.py
+++ jobs/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+from .models import Job
+
 
 def home(request):
-    return HttpResponse('Job Board Home')
+    jobs = Job.objects.all()
+    return render(request, 'jobs/job_list.html', {'jobs': jobs})
+
+
+def job_detail(request, pk):
+    job = get_object_or_404(Job, pk=pk)
+    return render(request, 'jobs/job_detail.html', {'job': job})
