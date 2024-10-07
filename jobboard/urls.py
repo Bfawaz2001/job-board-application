ef275680fed987df7fdc872d35de15342cf6428a
Bilal Fawaz <69845836+Bfawaz2001@users.noreply.github.com>
2024-10-07 01:46:21 +0100

    Fix logout error by using POST form for logout

--- jobboard/urls.py
+++ jobboard/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('jobs.urls')),
-    path('login/', auth_views.LoginView.as_view(), name='login'),  # Login page
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # Logout page
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), {'next_page': '/'}, name='logout'),  # Allow GET logout
 ]
