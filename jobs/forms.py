5240a669804814e075a3846f570bc2fbfae81814
Bilal Fawaz <69845836+Bfawaz2001@users.noreply.github.com>
2024-10-07 02:35:00 +0100

    Updated navigation bar layout and style job posting form. Improved navigation bar consistency and enhance job posting page with validation. Fix alignment of Post a Job and Logout, add date picker and improve error visibility on Post a Job page

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from .models import Job
+from django.forms.widgets import DateInput
 
 class JobForm(forms.ModelForm):
     class Meta:
         model = Job
         fields = ['title', 'company', 'description', 'location', 'employment_type', 'application_deadline']
+        widgets = {
+            'application_deadline': DateInput(attrs={'type': 'date'}),
+        }
